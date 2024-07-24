dba6cfdff58dc06a36577c7251bf3d002f8cb63c
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-07-24 11:22:28 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { useHomeStore } from "@/stores/homeStore";
+
+const store = useHomeStore();
+console.log(store.count);
+</script>
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex p-8 justify-center">
+    <input type="text" name="" id="" class="rounded border-1 border-gray-600" />
+  </div>
+</template>
+
+<script setup></script>
