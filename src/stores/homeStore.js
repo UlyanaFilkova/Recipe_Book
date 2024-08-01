97c87862def57229e42c0426f476eeaf9e74f79c
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-08-01 10:35:39 +0300

    get meals data by name

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -1,8 +1,16 @@
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
+import axiosClient from "@/axiosClient.js";
 
 export const useHomeStore = defineStore("homeStore", () => {
   const meals = ref([0, 1, 2]);
+  const searchedMeals = ref([]);
 
-  return { meals };
+  const searchMeal = async (keyword) => {
+    const response = await axiosClient.get(`search.php?s=${keyword}`);
+    searchedMeals.value = response.data.meals;
+    console.dir(searchedMeals.value);
+  };
+
+  return { meals, searchedMeals, searchMeal };
 });
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,19 +1,30 @@
 <template>
-  name
+  <div class="p-8">
+    <input
+      type="text"
+      name=""
+      id=""
+      v-model="keyword"
+      @change="searchMeal"
+      class="rounded border-1 border-gray-600 w-full"
+      placeholder="Search for meals"
+    />
+  </div>
+  <ul v-if="keyword">
+    <li v-for="meal in meals" :key="meal.id">{{ meal.strMeal }}</li>
+  </ul>
 </template>
 
 <script setup>
 import { useHomeStore } from "@/stores/homeStore";
-import { toRaw, onMounted, ref } from "vue";
+import { toRaw, onMounted, ref, computed } from "vue";
 import axiosClient from "@/axiosClient.js";
 
 const store = useHomeStore();
-const meals = store.meals;
-const ingredients = ref([]);
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-
-onMounted(async () => {
-  const response = await axiosClient.get("/list.php?i=list");
-  ingredients.value = response.data;
-});
+const keyword = ref("");
+const meals = computed(() => store.searchedMeals);
+function searchMeal() {
+  store.searchMeal(keyword.value);
+  console.dir(meals);
+}
 </script>
