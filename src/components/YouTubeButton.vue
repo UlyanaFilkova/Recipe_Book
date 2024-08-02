a9492a0eefb73598c507db16a4742a9463cba258
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-08-02 12:01:53 +0300

    display meal details and create YouTubeButton component

---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors"
+  >
+    <slot>YouTube</slot>
+  </a>
+</template>
+
+<script setup>
+import { useHomeStore } from "@/stores/homeStore";
+import { toRaw, onMounted, ref, computed, defineProps } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import axiosClient from "@/axiosClient.js";
+
+const props = defineProps({
+  href: String,
+});
+</script>
--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,18 +1,67 @@
 <template>
-  {{ meal }}
+  <div class="max-w-[800px] mx-auto p-8">
+    <h1 class="text-4xl font-bold mb-5 text-orange-500">{{ meal.strMeal }}</h1>
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" class="max-w-[100%]" />
+    <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
+      <div>
+        <strong class="font-bold">Category:</strong> {{ meal.strCategory }}
+      </div>
+      <div><strong class="font-bold">Area:</strong> {{ meal.strArea }}</div>
+      <div><strong class="font-bold">Tags:</strong> {{ meal.strTags }}</div>
+    </div>
+
+    <div class="my-3">
+      {{ meal.strInstructions }}
+    </div>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2">
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Ingredients</h2>
+        <ul>
+          <template v-for="(el, ind) of new Array(20)">
+            <li v-if="meal[`strIngredient${ind + 1}`]">
+              {{ ind + 1 }}. {{ meal[`strIngredient${ind + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Measures</h2>
+        <ul>
+          <template v-for="(el, ind) of new Array(20)">
+            <li v-if="meal[`strMeasure${ind + 1}`]">
+              {{ ind + 1 }}. {{ meal[`strMeasure${ind + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+      <div class="mt-4">
+        <YouTubeButton :href="meal.strYoutube">Go to YouTube </YouTubeButton>
+        <a
+          :href="meal.strSource"
+          target="_blank"
+          class="ml-3 px-3 py-2 rounded border-2 border-transparent text-indigo-600 transition-colors"
+        >
+          View Original Source
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 import { useHomeStore } from "@/stores/homeStore";
 import { toRaw, onMounted, ref, computed } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import axiosClient from "@/axiosClient.js";
+import YouTubeButton from "@/components/YouTubeButton.vue";
 
 const store = useHomeStore();
 const meal = ref({});
 const route = useRoute();
 
 onMounted(async () => {
-  const response = await axiosClient.get(`/list.php?i=${route.params.id}`);
+  const response = await axiosClient.get(`/lookup.php?i=${route.params.id}`);
+  console.dir(response.data.meals[0]);
   meal.value = response.data.meals[0] || {};
 });
 </script>
