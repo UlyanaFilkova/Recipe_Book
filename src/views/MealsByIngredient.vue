0c4a9a455ff0976ff128d00afefe1e4938fe3cd4
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-08-02 15:11:13 +0300

    add env

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,19 +1,44 @@
 <template>
-  ing
+  <div class="flex flex-col p-8 justify-center items-center">
+    <input
+      type="text"
+      name=""
+      id=""
+      class="rounded border-1 border-gray-600 w-full"
+      placeholder="Search for meals"
+    />
+
+    <div
+      v-if="keyword"
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full"
+    >
+      <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { useHomeStore } from "@/stores/homeStore";
-import { toRaw, onMounted, ref } from "vue";
-import axiosClient from "@/axiosClient.js";
+import { toRaw, onMounted, ref, computed, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import MealItem from "@/components/MealItem.vue";
 
 const store = useHomeStore();
-const meals = store.meals;
-const ingredients = ref([]);
+const meals = computed(() => store.mealsByLetter);
+const keyword = ref("");
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-onMounted(async () => {
-  const response = await axiosClient.get("/list.php?i=list");
-  ingredients.value = response.data;
+const route = useRoute();
+const router = useRouter();
+
+onMounted(() => {
+  keyword.value = route.params.letter;
+  if (keyword.value) {
+    store.searchMealsByLetter(keyword.value);
+  }
+});
+
+watch(route, () => {
+  store.searchMealsByLetter(route.params.letter);
 });
 </script>
